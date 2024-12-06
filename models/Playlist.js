d31d77fcffdda986a01af47aff872fe60f4145b5
Prince <pp275688@gmail.com>
2024-12-07 00:47:07 +0530

    first commit

---
+++ models/Playlist.js
@@ -0,0 +1,13 @@
+// Playlist.js
+const mongoose = require('mongoose');
+
+const playlistSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, default: '' },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    tracks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Track' }]
+}, { timestamps: true });
+
+const Playlist = mongoose.model('Playlist', playlistSchema);
+
+module.exports = Playlist;
---
+++ routes/notificationService.js
@@ -0,0 +1,14 @@
+// notificationService.js
+// Import necessary modules
+const NotificationService = require('./notificationService');
+
+// Function to send notification
+exports.sendNotification = async (userId, message) => {
+    try {
+        // Example: Send notification using a hypothetical notification service
+        await NotificationService.send(userId, message);
+        console.log(`Notification sent to user ${userId}: ${message}`);
+    } catch (error) {
+        throw new Error('Failed to send notification');
+    }
+};
